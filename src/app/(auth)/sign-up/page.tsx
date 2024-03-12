83dec3665b8ae51572da818f50a33171bf25cc12
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-12 20:30:09 +0800

    refactor: reuse layout instead of component

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import SignUpForm from './sign-up-form';
+
+export default function SignUp() {
+  return (
+    <>
+      <div className='space-y-4 rounded-lg border border-slate-500 p-4'>
+        <h1 className='text-center text-lg font-bold'>Sign-Up</h1>
+        <SignUpForm />
+      </div>
+      <p className='self-end'>
+        Have an account?
+        <Button variant='link' asChild className='ml-2 p-0'>
+          <Link href='/login'>Login</Link>
+        </Button>
+      </p>
+    </>
+  );
+}
