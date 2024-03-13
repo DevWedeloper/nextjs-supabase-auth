e764d7210a950f1ebe0e47b04156ba8ae5cee7a5
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-13 14:12:21 +0800

    feat/add-protected-page

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,9 @@
+export default function Protected() {
+  return (
+    <div className='flex h-screen items-center justify-center p-4'>
+      <h1 className='text-center text-lg font-bold'>
+        This route is protected!
+      </h1>
+    </div>
+  );
+}
