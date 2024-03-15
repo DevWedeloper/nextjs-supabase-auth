870bd337ca96942889878bd3438121bc7b47cda6
DevWedeloper <vicnathangabrielle@gmail.com>
2024-03-15 18:10:03 +0800

    refactor: use card wrapper

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,14 +1,15 @@
 import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import Link from 'next/link';
 import ResetPasswordForm from './reset-password-form';
 
 export default function ResetPassword() {
   return (
     <>
-      <div className='space-y-4 rounded-lg border border-slate-500 p-4'>
+      <Card className='space-y-4 rounded-lg border p-4'>
         <h1 className='text-center text-lg font-bold'>Reset Password</h1>
         <ResetPasswordForm />
-      </div>
+      </Card>
       <p className='self-end'>
         Have an account?
         <Button variant='link' asChild className='ml-2 p-0'>
